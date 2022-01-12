dce4395d7930426c50c92f6c61c4515d48f45079
Simon Ugor <simonugor@MacBook-Air-uzivatela-Simon.local>
2022-01-12 01:00:06 +0100

    whattherocket appstore redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/whattherocket',
+        destination: 'https://apps.apple.com/us/app/whattherocket/id1601871237',
+        permanent: true
+      },
+    ]
+  }
 }
