b9fcb95650889f1f0db4daff4ae2ac48c73d4a3e
Simon Ugor <simonugor@MacBook-Air-uzivatela-Simon.local>
2022-02-23 11:24:15 +0100

    Drop That Box redirect

--- pages/dropthatbox.js
+++ pages/dropthatbox.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import redirect from 'nextjs-redirect'
+import { useEffect } from 'react'
 
 Home.getInitialProps = (ctx) => {
     let isMobileView = (ctx.req
@@ -9,14 +11,36 @@ Home.getInitialProps = (ctx) => {
           /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
     )
 
+    let testMessage;
+
     let deviceInfo = ctx.req.headers['user-agent']
+    // if this includes "iphone" - open AppStore
+    // else if it includes "android" (or something like that) - open PlayStore
+    // else it must be a computer - do something else
+    
+    if (deviceInfo.includes("Mac")) {
+      testMessage = "ios"
+    } else {
+      testMessage = "android"
+    }
+    
 
     return {
-        deviceInfo: deviceInfo
+        deviceInfo: deviceInfo,
+        testMessage: testMessage
     }
   }
 
-function Home({deviceInfo}) {
+function Home({deviceInfo, testMessage}) {
+
+  useEffect(() => {
+    if(testMessage == "ios"){
+      window.location.assign("https://apps.apple.com/app/drop-that-box/id1609472916") 
+    } else {
+      window.location.assign("https://play.google.com/store/apps/details?id=com.coldendev.dropthatbox")
+    }
+    
+  })
 
   return (
     <div className={styles.container}>
@@ -25,8 +49,6 @@ function Home({deviceInfo}) {
         <meta name="description" content="Drop That Box - mobile game developed by ColdenDev" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>{deviceInfo}</h1>
 
     </div>
   )
