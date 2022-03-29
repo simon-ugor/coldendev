0fa59887be85c1cbb27e98c83a42761efbbba722
Simon Ugor <simonugor@MacBook-Air-uzivatela-Simon.local>
2022-03-29 02:35:38 +0200

    Privacy policy update

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -21,19 +21,19 @@ export default function Home() {
         <div className={styles.grid}>
           <div>
             <h2>Privacy Policy</h2>
-            <p>Simon Ugor built the WhatTheRocket app as a Free app. This SERVICE is provided by Simon Ugor at no cost and is intended for use as is.
+            <p>Simon Ugor built apps WhatTheRocket, Drop That Box and Planets Dodge as Free apps. This SERVICE is provided by Simon Ugor at no cost and is intended for use as is.
             This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
             If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.
-            The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at WhatTheRocket unless otherwise defined in this Privacy Policy.</p>
+            The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at WhatTheRocket, Drop That Box and Planets Dodge unless otherwise defined in this Privacy Policy.</p>
             <h2>Information Collection and Use</h2>
             <p>For a better experience, while using our Service, I may require you to provide us with certain personally identifiable information. The information that I request will be retained on your device and is not collected by me in any way.
-            The app does use third-party services that may collect information used to identify you.
-            Link to the privacy policy of third-party service providers used by the app - <a href='https://unity3d.com/legal/privacy-policy'>Unity</a></p>
+            The apps does use third-party services that may collect information used to identify you.
+            Link to the privacy policy of third-party service providers used by the apps - <a href='https://unity3d.com/legal/privacy-policy'>Unity</a></p>
             <h2>Log Data</h2>
-            <p>I want to inform you that whenever you use my Service, in a case of an error in the app I collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.</p>
+            <p>I want to inform you that whenever you use my Service, in a case of an error in apps I collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.</p>
             <h2>Cookies</h2>
             <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device&apos;s internal memory.
-            This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+            This Service does not use these “cookies” explicitly. However, apps may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
             <h2>Service Providers</h2>
             <p>I may employ third-party companies and individuals due to the following reasons: To faciliate our Service; To provide the Service on our behalf; To perform Service-related services; To assist us in analyzing how our Service is used. I want to inform users of this Service that these third parties have access to their Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
             <h2>Security</h2>
