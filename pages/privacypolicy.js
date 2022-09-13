29ecc1c55f2f62697c97ad697435c47867aaf77d
Simon Ugor <simonugor@MacBook-Air-uzivatela-Simon.local>
2022-09-13 23:05:23 +0200

    hackthegame

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -21,7 +21,7 @@ export default function Home() {
         <div className={styles.grid}>
           <div>
             <h2>Privacy Policy</h2>
-            <p>Simon Ugor built apps WhatTheRocket, Drop That Box and Planets Dodge as Free apps. This SERVICE is provided by Simon Ugor at no cost and is intended for use as is.
+            <p>Simon Ugor built apps WhatTheRocket, Drop That Box, Planets Dodge and Hack The Game as Free apps. This SERVICE is provided by Simon Ugor at no cost and is intended for use as is.
             This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
             If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.
             The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at WhatTheRocket, Drop That Box and Planets Dodge unless otherwise defined in this Privacy Policy.</p>
